883757f289de2453100bd5702cb8154fcc93cf91
cherryDevBomb <turcan.olguta@gmail.com>
2021-11-02 18:28:44 +0000

    change to java11 compatible collections

--- src/main/java/analyzer/controller/Controller.java
+++ src/main/java/analyzer/controller/Controller.java
@@ -1,35 +1,23 @@
 package analyzer.controller;
 
 import analyzer.model.WirelessNetworkInfo;
-import analyzer.observer.Observer;
 import analyzer.service.PacketCaptureService;
-import com.sun.javafx.collections.ObservableListWrapper;
+import javafx.collections.FXCollections;
 import javafx.application.Platform;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableView;
 
-import java.util.ArrayList;
 import java.util.concurrent.Executors;
 
-public class Controller implements Observer {
+public class Controller {
 
     @FXML private TableView<WirelessNetworkInfo> tableView;
-
-    private ObservableList<WirelessNetworkInfo> observableList;
 
     private final PacketCaptureService pCapService = new PacketCaptureService();
 
     @FXML private void initialize() {
-        pCapService.registerObserver(this);
-
-        observableList = new ObservableListWrapper<>(new ArrayList<>(pCapService.getNetworks().values()));
-        Platform.runLater(() -> tableView.setItems(observableList));
+        Platform.runLater(() -> tableView.setItems(FXCollections.observableArrayList​(pCapService.getNetworks().values())));
         Executors.newSingleThreadExecutor().execute(pCapService::capture);
     }
 
-    @Override
-    public void updateView() {
-        observableList.setAll(pCapService.getNetworks().values());
-    }
 }
