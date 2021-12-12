62785ef387198b543f80bb3e06e18d5fd8f6e3c0
cherryDevBomb <turcan.olguta@gmail.com>
2021-12-12 22:53:43 +0200

    fix icon binding

--- src/main/java/analyzer/controller/Controller.java
+++ src/main/java/analyzer/controller/Controller.java
@@ -8,18 +8,27 @@ import analyzer.util.Observer;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class Controller implements Observer {
 
     @FXML
     private TableView<WirelessNetworkInfo> tableView;
 
+    @FXML
+    private TableColumn<WirelessNetworkInfo, Image> iconColumn;
+
     private final PacketCaptureService pCapService = new PacketCaptureService();
 
     @FXML
     private void initialize() {
         pCapService.registerObserver(this);
+        bindIconColumn();
         Platform.runLater(() -> tableView.setItems(FXCollections.observableArrayList(pCapService.getNetworks().values())));
         Executors.newSingleThreadExecutor().execute(pCapService::capture);
     }
@@ -27,5 +36,27 @@ public class Controller implements Observer {
     @Override
     public void updateView() {
         tableView.setItems(FXCollections.observableArrayList(pCapService.getNetworks().values()));
+    }
+
+    private void bindIconColumn() {
+        iconColumn.setCellFactory(param -> {
+            //Set up the ImageView
+            final ImageView imageview = new ImageView();
+            imageview.setFitHeight(35);
+            imageview.setFitWidth(35);
+
+            //Set up the Table
+            TableCell<WirelessNetworkInfo, Image> cell = new TableCell<WirelessNetworkInfo, Image>() {
+                public void updateItem(Image item, boolean empty) {
+                    if (item != null) {
+                        imageview.setImage(item);
+                    }
+                }
+            };
+            // Attach the imageview to the cell
+            cell.setGraphic(imageview);
+            return cell;
+        });
+        iconColumn.setCellValueFactory(new PropertyValueFactory<>("icon"));
     }
 }
